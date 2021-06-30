34cac098b939b86d20f63bbc6d863ab3998eaf42
unknown <gichtyou7@naver.com>
2021-06-30 20:49:49 +0900

    2021/06/30

--- Once/1003.py
+++ Once/1003.py
@@ -1,3 +1,4 @@
+d = {}
 def fibo(n):
     if n == 0:
         d[0] = [0,1,0]
@@ -10,9 +11,9 @@ def fibo(n):
     else:
         d[n] = [fibo(n-1) + fibo(n-2),d[n-1][1] + d[n-2][1], d[n-1][2] +d[n-2][2]]
         return d[n][0]
-T = int(input())
+
 fibo(40)
-d = {}
+T = int(input())
 for i in range(T):
     X = int(input())
     print(d[X][1],d[X][2])
